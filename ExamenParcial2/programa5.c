098caadcac6b2e15382b91f4f6eed627a807f637
radame23 <141948584+radame23@users.noreply.github.com>
2023-10-05 09:35:40 -0600

    Add files via upload

---
+++ ExamenParcial2/programa5.c
@@ -0,0 +1,23 @@
+//año bisiesto 
+#include <stdio.h>
+
+int main(){
+	
+	int a=0;
+	
+	printf("Introduce un año:");
+	scanf("%d", &a);
+	
+	while(!((a%4==0 && a%100!=0 && a>0) || a%400==0 ))
+	{
+		printf("este año no es bisiesto\n\n");
+		printf("vuelve a poner otro año:\n");
+		scanf("%d", &a);
+	}
+	
+			printf("esta año si es bisiesto\n");
+	
+		
+	return 0; 
+}
+
